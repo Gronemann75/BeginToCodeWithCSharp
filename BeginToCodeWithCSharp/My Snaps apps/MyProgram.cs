eca9cae2f7c3c328cd1ec126dc5187ff84f05251
macma <macma@DESKTOP-CPBVJR0>
2016-09-22 08:45:40 +0200

    Add project files.

---
+++ BeginToCodeWithCSharp/My Snaps apps/MyProgram.cs
@@ -0,0 +1,12 @@
+﻿using SnapsLibrary;
+
+public class MyProgram
+{
+    public void StartProgram()
+    {
+        SnapsEngine.SetTitleString("Begynd at kode med C#");
+        SnapsEngine.DisplayString("Velkommen til en verden fuld af Snaps");
+    }
+}
+
+
